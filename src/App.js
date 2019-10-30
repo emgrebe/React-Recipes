953e7951cedda326079e6b5ed029b0a10b3a6825
Emily Landgrebe <emilandgrebe@gmail.com>
2019-10-30 17:25:14 -0500

    set up .env for api key and api id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App =() => {
+
+  const API_ID = process.env.REACT_APP_API_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  return(
+    <div>
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
