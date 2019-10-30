66e664b90e543b2eb63b4cc5bbcb8894a0638345
Emily Landgrebe <emilandgrebe@gmail.com>
2019-10-30 17:29:36 -0500

    make form for input and button

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ const App =() => {
 
   const API_ID = process.env.REACT_APP_API_ID;
   const API_KEY = process.env.REACT_APP_API_KEY;
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`;
 
   return(
-    <div>
-      <h1>Hello React</h1>
+    <div className="App">
+      <form className="Search-Form">
+        <input type="text"></input>
+        <button type="submit">Get Recipe</button>
+      </form>
     </div>
   )
 }
